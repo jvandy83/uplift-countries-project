90bc906eea2772be75a8b713ae04adc8969ed2d4
jvandy83 <vanthedev@gmail.com>
2025-04-28 09:51:45 -0600

    test: set up Vitest and React Testing Library

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Vite + React', () => {
+  render(<App />);
+  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+});
+
